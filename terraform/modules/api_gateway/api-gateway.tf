e244c12538018170768781bcfd76189e664ae8c2
venu <venunature.reddy5@gmail.com>
2024-12-17 14:02:51 +0530

    fixing the route

--- terraform/modules/api_gateway/api-gateway.tf
+++ terraform/modules/api_gateway/api-gateway.tf
@@ -1,22 +1,22 @@
-resource "aws_apigatewayv2_api" "this" {
+resource "aws_apigatewayv2_api" "http" {
   name          = var.api_name
   protocol_type = "HTTP"
 }
 
 resource "aws_apigatewayv2_integration" "lambda" {
-  api_id           = aws_apigatewayv2_api.this.id
+  api_id           = aws_apigatewayv2_api.http.id
   integration_type = "AWS_PROXY"
   integration_uri  = var.lambda_function_arn
 }
 
 resource "aws_apigatewayv2_route" "default" {
-  api_id    = aws_apigatewayv2_api.this.id
-  route_key = "ANY /tracks"
-  target    = aws_apigatewayv2_integration.lambda.id
+  api_id    = aws_apigatewayv2_api.http.id
+  route_key = "POST /tracks"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
 }
 
 resource "aws_apigatewayv2_stage" "default" {
-  api_id      = aws_apigatewayv2_api.this.id
+  api_id      = aws_apigatewayv2_api.http.id
   name        = var.stage_name
   auto_deploy = true
 }
