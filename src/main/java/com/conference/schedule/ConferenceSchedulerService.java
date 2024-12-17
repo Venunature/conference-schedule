732bdce98d4b6175eda26dfe830b4ca1f8c6eb05
venu <venunature.reddy5@gmail.com>
2024-12-17 15:00:20 +0530

    remove the unwanted code

--- src/main/java/com/conference/schedule/ConferenceSchedulerService.java
+++ src/main/java/com/conference/schedule/ConferenceSchedulerService.java
@@ -15,11 +15,6 @@ public class ConferenceSchedulerService {
     static final int AFTERNOON_SESSION_MIN = 180;
     static final int AFTERNOON_SESSION_MAX = 240;
 
-//    public List<String> schedule(List<String> input) {
-//        List<Talk> talks = parseInput(input);
-//        List<Track> tracks = createTracks(talks);
-//        return formatOutput(tracks);
-//    }
 
     public List<Track> schedule(List<String> input) throws NumberInTitleException {
         List<Talk> talks = parseInput(input);
@@ -91,33 +86,6 @@ public class ConferenceSchedulerService {
             }
         }
         return session;
-    }
-
-    private List<String> formatOutput(List<Track> tracks) {
-        List<String> output = new ArrayList<>();
-        LocalTime morningStart = LocalTime.of(9, 0);
-        LocalTime afternoonStart = LocalTime.of(13, 0);
-        for (int i = 0; i < tracks.size(); i++) {
-            Track track = tracks.get(i);
-            output.add("Track " + (i + 1) + ":");
-            output.addAll(formatSession(track.getMorningSession(), morningStart));
-            output.add(track.getLunch()+" 60 mins");
-            output.addAll(formatSession(track.getAfternoonSession(), afternoonStart));
-            output.add("Networking Event: "+track.getNetworkEventStart());
-        }
-        return output;
-    }
-
-    private List<String> formatSession(List<Talk> session, LocalTime startTime) {
-        List<String> output = new ArrayList<>();
-        LocalTime currentTime = startTime;
-        if(session!=null){
-            for (Talk talk : session) {
-                output.add(currentTime + " - " + currentTime.plusMinutes(talk.duration) + ": " + talk.title);
-                currentTime = currentTime.plusMinutes(talk.duration);
-            }
-        }
-        return output;
     }
 
 }
